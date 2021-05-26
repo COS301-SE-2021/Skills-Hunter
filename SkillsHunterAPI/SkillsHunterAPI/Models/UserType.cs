ce73873d9c8493cb10aac1e4d800b7023f2ad633
Mxolisi Mkanzi <u18059326@tuks.co.za>
2021-05-26 22:04:28 +0200

    Add files via upload

---
+++ SkillsHunterAPI/SkillsHunterAPI/Models/UserType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace SkillsHunterAPI.Models
+{
+    public class UserType
+    {
+        public UserType() { }
+
+        public int Id { get; set; }
+
+        public string Description { get; set; }
+    }
+}
