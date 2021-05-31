e878ed3e4136c581f496d33173db772925d67743
Joseph-Makgopa <u18166793@tuks.co.za>
2021-05-31 16:56:48 +0200

    home component added

--- SkillsHunterFrontend/src/app/home/home.component.ts
+++ SkillsHunterFrontend/src/app/home/home.component.ts
@@ -1,26 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { MatDialog,MatDialogConfig } from '@angular/material/dialog';
-import { CreateprojectComponent } from './../createproject/createproject.component';
+import { Project } from './Project';
+import { Projects } from './mock-projects';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
 
-  constructor(private _router: Router,private dialog:MatDialog) { }
+export class HomeComponent implements OnInit {
+  title :string = 'Projects';
+   _projects:Project[] = Projects;
+   
+  constructor() { }
 
   ngOnInit(): void {
   }
 
-  //this function will open a popup with fields for collecting project info
-  create(){
-    const configDialog=new MatDialogConfig();
-    configDialog.backdropClass="backGround";
-    // configDialog.width='700px';
-    // configDialog.height='400px';
-    this.dialog.open(CreateprojectComponent ,configDialog);
-  }
 }
