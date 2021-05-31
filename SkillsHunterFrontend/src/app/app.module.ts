94767247b43987152c8cdc2c4c04f472d9cb3ced
Thuso-Khoza <u17088799@tuks.co.za>
2021-05-30 22:29:29 -0700

    incution of modules and components

--- SkillsHunterFrontend/src/app/app.module.ts
+++ SkillsHunterFrontend/src/app/app.module.ts
@@ -5,10 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from './login/login.component';
 import { CreateprojectComponent } from './createproject/createproject.component';
 import { UpdateprojectComponent } from './updateproject/updateproject.component';
 import { HomeComponent } from './home/home.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ProjectCRUDService } from './services/project-crud.service';
 
 @NgModule({
   declarations: [
@@ -22,9 +25,12 @@ import { HomeComponent } from './home/home.component';
     BrowserModule,
     AppRoutingModule,
     NoopAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ProjectCRUDService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
