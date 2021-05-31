69eebf0b87b7545684955fb92a90195c6218d434
MxolisiMkanzi <u18059326@tuks.co.za>
2021-06-01 00:49:56 +0200

    Project creation test created

--- SkillsHunterAPI/SkillsHunterAPIUnitTest/ProjectUnitTest.cs
+++ SkillsHunterAPI/SkillsHunterAPIUnitTest/ProjectUnitTest.cs
@@ -1,7 +1,7 @@
 using System;
 using Xunit;
-using SkillsHunterAPI.Controllers;
-using SkillsHunterAPI.Repositories;
+using SkillsHunterAPI.Models;
+using System.Collections.Generic;
 
 namespace SkillsHunterAPIUnitTest.Tests
 {
@@ -24,8 +24,21 @@ namespace SkillsHunterAPIUnitTest.Tests
         [Fact]
         public void TestCreateProject() //Testing the creation of projects
         {
+            Project project = new Project();
+            project.Description = "Web development";
+            project.Location = "Hatfield";
+            project.Name = "Skills Hunter";
+            project.Owner = "Mxolisi";
+            project.Skills = "HTML,CSS";
+            project.OpenForApplication = true;
+            project.Industry = "CS";
 
-            
+            var actionResults = projectController.CreateProject(project);
+
+            var results = actionResults.Result;
+            var resultProject = results.Value as Project;
+
+            Assert.Equal(project, resultProject);
         }
 
         [Fact]
