6e0e63d6c5051d471ebb7d2048be91b3a982c6d9
MxolisiMkanzi <u18059326@tuks.co.za>
2021-06-02 21:54:02 +0200

    Update ProjectUnitTest.cs

--- SkillsHunterAPI/SkillsHunterAPIUnitTest/ProjectUnitTest.cs
+++ SkillsHunterAPI/SkillsHunterAPIUnitTest/ProjectUnitTest.cs
@@ -14,13 +14,13 @@ namespace SkillsHunterAPIUnitTest.Tests
     {
         //TESTING ONLY THE PROJECT SUBSYSTEM
 
-        IProjectRepository projectRepository;
-        ProjectController projectController;
+        IProjectRepository _projectRepo;
+        ProjectController _controller;
 
         public ProjectUnitTest()
         {
-            projectRepository = A.Fake<IProjectRepository>();
-            projectController = new ProjectController(projectRepository);
+            _projectRepo = A.Fake<IProjectRepository>();
+            _controller = new ProjectController(_projectRepo);
         }
 
 
@@ -34,40 +34,13 @@ namespace SkillsHunterAPIUnitTest.Tests
         [Fact]
         public void testCreateProject() //Testing the creation of projects
         {
-            Project project = new Project();
-            project.Description = "Web development";
-            project.Location = "Hatfield";
-            project.Name = "Skills Hunter";
-            project.Owner = "Mxolisi";
-            project.Skills = "HTML,CSS";
-            project.OpenForApplication = true;
-            project.Industry = "CS";
-
-            var actionResults = projectController.CreateProject(project);
-
-            var results = actionResults.Result;
-            var resultProject = results.Value as Project;
-
-            Assert.Equal(project, resultProject);
+            
         }
 
         [Fact]
         public async Task testGetProjects()    //testing the function that retrieves projects from the database
         {
-            //var testController = new
-            var count = 6;
-            var fakeProjects = A.CollectionOfFake<Project>(count);
-            A.CallTo(() => projectRepository.GetProjects()).Returns((fakeProjects));
-
-            //Act
-
-            var IEnumerable = await projectController.GetProjects();
-
-            //Assert
-
-            var result = IEnumerable.ToList();
-            var returnProjects = result.Count();
-            Assert.Equal(count, returnProjects);
+           
         }
 
         [Fact]
@@ -79,7 +52,7 @@ namespace SkillsHunterAPIUnitTest.Tests
         [Fact]
         public void testDeleteProject()    //testing the function that deletes only one project
         {
-            //var testController = new 
+
         }
     }
 }
