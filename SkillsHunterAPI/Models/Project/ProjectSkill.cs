f5b58ae0d4d61a1a7ea72e088c743acc312d8ae7
MxolisiMkanzi <u18059326@tuks.co.za>
2021-06-10 01:00:58 +0200

    ProjectSkillID added

--- SkillsHunterAPI/Models/Project/ProjectSkill.cs
+++ SkillsHunterAPI/Models/Project/ProjectSkill.cs
@@ -3,8 +3,6 @@ namespace SkillsHunterAPI.Models.Project
 {
     public class ProjectSkill
     {
-        public ProjectSkill()
-        {
-        }
+        public int ProjectSkillID { get; set; }
     }
 }
