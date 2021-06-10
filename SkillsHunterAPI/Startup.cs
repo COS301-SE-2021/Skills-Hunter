653aa8bbadd16c9c1e65c943a175e263b79a7df9
MxolisiMkanzi <u18059326@tuks.co.za>
2021-06-10 14:06:49 +0200

    Added CORS configurations

--- SkillsHunterAPI/Startup.cs
+++ SkillsHunterAPI/Startup.cs
@@ -1,12 +1,21 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using SkillsHunterAPI.Data;
+using SkillsHunterAPI.Models;
+using SkillsHunterAPI.Repositories;
 using SkillsHunterAPI.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace SkillsHunterAPI
 {
@@ -30,6 +39,15 @@ namespace SkillsHunterAPI
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "SkillsHunterAPI", Version = "v1" });
             });
+
+            services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(
+                builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+                // options.AddPolicy("mypolicy", options => options.WithHeaders());
+            }
+
+            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,9 +60,12 @@ namespace SkillsHunterAPI
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "SkillsHunterAPI v1"));
             }
 
+
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors();
 
             app.UseAuthorization();
 
