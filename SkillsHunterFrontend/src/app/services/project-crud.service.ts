8bec14717a35d50351fe4737147c13d55aa94ef0
Thuso-Khoza <u17088799@tuks.co.za>
2021-06-13 00:56:49 -0700

    creation of deleteProject function
    creation of deleteProject function in project crud api

--- SkillsHunterFrontend/src/app/services/project-crud.service.ts
+++ SkillsHunterFrontend/src/app/services/project-crud.service.ts
@@ -19,12 +19,20 @@ export class ProjectCRUDService {
         return this.httpclient.post("http://localhost:5000/api/Project/createProject",formData);
   }
 
+  //external api to update project is called here
   updateProject(formData:project):Observable<any>
   {
     return this.httpclient.post("http://localhost:5000/api/Project/createProject",formData);
 
   }
 
+  //external api to delete project is called here
+  deleteProject(formData:project):Observable<any>
+  {
+    return this.httpclient.post("http://localhost:5000/api/Project/createProject",formData)
+  }
+  
+  //external api to read project is called here
   getProjects():Observable<Project[]>{
         return this.httpclient.get<Project[]>('http://localhost:5000/api/Project/getProjects'); 
     }
