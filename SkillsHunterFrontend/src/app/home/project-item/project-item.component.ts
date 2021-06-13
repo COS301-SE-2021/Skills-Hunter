7fac69b816d6acce6bf8ef5217fb7ca1c7cdfe37
Thuso-Khoza <u17088799@tuks.co.za>
2021-06-13 01:00:09 -0700

    correction of definition
    correction of definition of project data types

--- SkillsHunterFrontend/src/app/home/project-item/project-item.component.ts
+++ SkillsHunterFrontend/src/app/home/project-item/project-item.component.ts
@@ -1,7 +1,11 @@
+import { MatIconModule } from '@angular/material/icon';
 import { Component, OnInit,Input } from '@angular/core';
 import { UpdateprojectComponent } from './../../updateproject/updateproject.component';
 import { MatDialog,MatDialogConfig } from '@angular/material/dialog';
-import { Project} from '../Project';
+import { project } from '../../classes/project';
+import { projectService } from '../../services/project-edit.service';
+import {ProjectCRUDService} from '../../services/project-crud.service' 
+
 @Component({
   selector: 'app-project-item',
   templateUrl: './project-item.component.html',
@@ -9,18 +13,54 @@ import { Project} from '../Project';
 })
 export class ProjectItemComponent implements OnInit {
   panelOpenState :boolean = false;
-  @Input() _project:Project = {id:-1,name:'',description:'',industry:'',owner:'',location:'',skills:'',openForApplication:false};
+  @Input() _project:project = {
+                              Id:-1,
+                              Name:'',
+                              Description:'',
+                              Industry:'' ,
+                              Owner:'',
+                              Location:'',
+                              Skill:[] ,
+                              OpenForApplication:false};
 
-  constructor(private dialog:MatDialog) { }
+  constructor(private dialog:MatDialog,private projectData:projectService,private projectCrud: ProjectCRUDService) { }
 
   ngOnInit(): void {
+
   }
 
-  update(){
+  get getProjectInfo():project
+  {
+    return this.projectData.projectBeingedited;
+  }
+  //this sets project service which holds the data which will be displayed in the update dialog
+  set setProjectInfo(project:project)
+  {
+    this.projectData.projectBeingedited = project;
+  }
+
+  update(project){
+
+    this.setProjectInfo=project;
     const configDialog=new MatDialogConfig();
     configDialog.backdropClass="backGround";
     configDialog.width='40%';
     configDialog.height='80%';
     this.dialog.open(UpdateprojectComponent ,configDialog);
+   
+  }
+  delete(project)
+  {
+    if(confirm(`Are you sure to delete ${project.Name}`))
+    {
+      document.getElementById(project.Id).style.display = "none";
+      
+       //the service is called below 
+      this.projectCrud.createProject(project)//change so it calls update
+      .subscribe(
+        data=>{
+          console.log('Response post', data);
+       });
+    }
   }
 }
