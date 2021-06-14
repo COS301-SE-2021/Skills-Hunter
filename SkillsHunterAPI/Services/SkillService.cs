d51bc1b18f737d7cd5ec5a041ebad203b21cf723
Sboniso Masilela <u10416260@tuks.co.za>
2021-06-14 16:38:17 +0200

    ADDED SKILL CLASS AND INTERFACE

---
+++ SkillsHunterAPI/Services/SkillService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SkillsHunterAPI.Services
+{
+    public class SkillService
+    {
+
+    }
+}
