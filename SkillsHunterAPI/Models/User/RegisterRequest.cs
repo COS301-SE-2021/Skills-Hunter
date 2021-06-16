fa08330a31fb62b2e6e3de6939ddcf54a91d55ad
MxolisiMkanzi <u18059326@tuks.co.za>
2021-06-16 22:16:53 +0200

    Modified code structure

--- SkillsHunterAPI/Models/User/RegisterRequest.cs
+++ SkillsHunterAPI/Models/User/RegisterRequest.cs
@@ -1,8 +1,6 @@
 using System;
 using SkillsHunterAPI.Models.User;
 
-
-
 namespace SkillsHunterAPI.Models.User
 {
     //This class will be used to contain all the User data that is sent through with a Project creation or update request
--- SkillsHunterAPI/Models/User/RegisterResponse.cs
+++ SkillsHunterAPI/Models/User/RegisterResponse.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace SkillsHunterAPI.Models.User
 {
     //This class will be used to contain all the User data that is sent through with a Project creation or update request
