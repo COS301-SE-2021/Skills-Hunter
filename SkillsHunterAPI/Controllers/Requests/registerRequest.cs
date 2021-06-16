7e85b065d90e2c17f77bcb2a5b633a9cc0a5de7c
Joseph-Makgopa <u18166793@tuks.co.za>
2021-06-16 21:29:41 +0200

    updated registerRequest

--- SkillsHunterAPI/Controllers/Requests/registerRequest.cs
+++ SkillsHunterAPI/Controllers/Requests/registerRequest.cs
@@ -3,17 +3,17 @@ using SkillsHunterAPI.Models.User;
 namespace SkillsHunterAPI.Controllers.Requests
 {
     //This class will be used to contain all the User data that is sent through with a Project creation or update request
-    public class registerRequest
+    public class RegisterRequest
     {
-        public string firstName { get; set; }
-        public string lastName {get;set;}
-        public string password{get; set;}
-        public string email{get;set;}
-        public string phoneNumber{get;set;}
-        public DateTime startDate{get;set;}
-        public Boolean openForWork{get;set;}
-        public UserType userType{get;set;}
-        public Guid[] skills{get;set;}
-        public WorkExperience[] experience{get;set;}
+        public string FirstName { get; set; }
+        public string LastName {get;set;}
+        public string Password{get; set;}
+        public string Email{get;set;}
+        public string PhoneNumber{get;set;}
+        public DateTime StartDate{get;set;}
+        public Boolean OpenForWork{get;set;}
+        public UserType UserType{get;set;}
+        public Guid[] Skills{get;set;}
+        public WorkExperience[] Experience{get;set;}
     }
 }
