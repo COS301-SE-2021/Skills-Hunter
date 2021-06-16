4f3ce6100213de79548845ca86028f44e8fbbe11
Joseph-Makgopa <u18166793@tuks.co.za>
2021-06-16 21:35:43 +0200

    updated registerResponse

--- SkillsHunterAPI/Controllers/Responses/registerResponse.cs
+++ SkillsHunterAPI/Controllers/Responses/registerResponse.cs
@@ -1,12 +1,12 @@
 namespace SkillsHunterAPI.Controllers.Responses
 {
     //This class will be used to contain all the User data that is sent through with a Project creation or update request
-    public class registerResponse
+    public class RegisterResponse
     {
-        public bool success{get;set;}
-        public string[] errors{get;set;}
-        public registerResponse(){
-            errors = new string[0];
+        public bool Success{get;set;}
+        public string[] Errors{get;set;}
+        public RegisterResponse(){
+            Errors = new string[0];
         }
     }
 }
