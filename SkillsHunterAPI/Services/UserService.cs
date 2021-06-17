4460575c33d867c71c50cd46b7a59f4ddb75b805
Joseph-Makgopa <u18166793@tuks.co.za>
2021-06-17 12:11:45 +0200

    implemented getUser and getAllUser

--- SkillsHunterAPI/Services/UserService.cs
+++ SkillsHunterAPI/Services/UserService.cs
@@ -9,11 +9,14 @@ namespace SkillsHunterAPI.Services
     public class UserService: IUserService
     {
         private SignInManager<User> _signManager; 
-        private UserManager<User> _userManager; 
-        public UserService(UserManager<User> userManager, SignInManager<User> signManager)
+        private UserManager<User> _userManager;
+        private readonly ApplicationDbContext _context;
+
+        public UserService(UserManager<User> userManager, SignInManager<User> signManager,ApplicationDbContext context)
         {
             _signManager = signManager;
             _userManager = userManager;
+            _context = context;
         }
 
         public async Task<RegisterResponse> Register(RegisterRequest request)
@@ -55,7 +58,7 @@ namespace SkillsHunterAPI.Services
 
         public async Task<LogOutResponse> LogOut(LogOutRequest request)
         {
-
+            
         }
 
         public async Task<UpdateResponse> UpdateUser(UpdateRequest request)
@@ -70,12 +73,24 @@ namespace SkillsHunterAPI.Services
 
         public async Task<GetAllResponse> GetAllUsers(GetAllRequest request)
         {
-
+            var result =  await _context.Users.ToListAsync();
+            GetAllResponse response = new GetAllResponse(); 
+            
+            response.Accounts = result.ToArray();
+            response.Success = true;
+            
+            return response;
         }
 
         public async Task<GetResponse> GetUser(GetUserRequest request)
         {
+            var result = await _context.Users.FindAsync(request.UserId);
+            GetResponse response = new GetResponse();
 
+            response.Account = result;
+            response.Success = true;
+
+            return response;
         }
     }
 }
