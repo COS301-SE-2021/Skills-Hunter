0d5c2b8885c0da90a80c11fed2fec56d374738cf
MxolisiMkanzi <u18059326@tuks.co.za>
2021-06-17 23:01:44 +0200

    The name and surname fields updated on the registerRequest class

--- SkillsHunterAPI/Models/User/RegisterRequest.cs
+++ SkillsHunterAPI/Models/User/RegisterRequest.cs
@@ -5,8 +5,8 @@ namespace SkillsHunterAPI.Models.User
     //This class will be used to contain all the User data that is sent through with a Project creation or update request
     public class RegisterRequest
     {
-        public string FirstName {get; set; }
-        public string LastName {get;set;}
+        public string Name {get; set; }
+        public string Surname {get;set;}
         public string Email {get; set;}
         public string Password {get; set;}
         public UserType UserType {get; set;}
