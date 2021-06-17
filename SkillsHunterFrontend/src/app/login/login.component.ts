5b6b7850849189c78b53c0deb2370b866250333b
Thuso-Khoza <u17088799@tuks.co.za>
2021-06-17 00:36:53 -0700

    log in ts
    log in functionalist in ts

--- SkillsHunterFrontend/src/app/login/login.component.ts
+++ SkillsHunterFrontend/src/app/login/login.component.ts
@@ -11,6 +11,27 @@ export class LoginComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.buttonLogin = "";
+
+    document.getElementById("tool").style.display = "none";
   }
 
+  hide = true;// for hiding password in UI
+  _username:string;
+  _password:string;
+  _match:boolean;
+  buttonLogin:string;
+
+  LoginForm = new FormGroup({  
+    email: new FormControl('', [Validators.required,Validators.email]) ,  
+    password: new FormControl('', [Validators.required]),
+  });
+
+  
+
+  onSubmit() 
+  {
+    this.buttonLogin="clicked";
+
+  }
 }
