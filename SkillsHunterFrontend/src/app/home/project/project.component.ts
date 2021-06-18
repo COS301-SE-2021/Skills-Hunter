9d295893f682787623789193d8821f39a70a7437
Tshepang <tshepangmog10@gmail.com>
2021-06-18 05:18:19 +0200

    Created new project card

--- SkillsHunterFrontend/src/app/home/project/project.component.ts
+++ SkillsHunterFrontend/src/app/home/project/project.component.ts
@@ -1,27 +1,23 @@
-import { Component, OnInit,Input } from '@angular/core';
-import { project } from '../../classes/project';
+import { Component, OnInit, Input } from '@angular/core';
+import { Project } from '../../classes/Project';
 
 @Component({
   selector: 'app-project',
   templateUrl: './project.component.html',
-  styleUrls: ['./project.component.scss']
+  styleUrls: ['./project.component.scss'],
 })
-
 export class ProjectComponent implements OnInit {
-  @Input() _project:project = {
-    Id:-1,
-    Name:'',
-    Description:'',
-    Industry:'',
-    Owner:'',
-    Location:'',
-    Skill:[] ,
-    OpenForApplication:false};  
-  
-  constructor() { 
-  }
+  @Input() _project: Project = {
+    id: -1,
+    Name: '',
+    Description: '',
+    Owner: '',
+    Location: '',
+    Skill: [],
+    OpenForApplication: false,
+  };
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
