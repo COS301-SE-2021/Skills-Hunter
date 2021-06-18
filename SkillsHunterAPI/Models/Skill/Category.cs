5c2020e881db8a8cef192f340cc31d3c6fcf9fe8
Joseph-Makgopa <u18166793@tuks.co.za>
2021-06-18 22:15:23 +0200

    updated category model and added AdminController file

--- SkillsHunterAPI/Models/Skill/Category.cs
+++ SkillsHunterAPI/Models/Skill/Category.cs
@@ -4,5 +4,7 @@ namespace SkillsHunterAPI.Models
     public class Category
     {
         public Guid CategoryId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
     }
 }
