80c772f14500b5f56cfe58a9b3118f8a726c18d7
MxolisiMkanzi <u18059326@tuks.co.za>
2021-06-19 23:10:40 +0200

    Defined the Application class
    Added more fields on the application table

--- SkillsHunterAPI/Models/Project/Application.cs
+++ SkillsHunterAPI/Models/Project/Application.cs
@@ -4,5 +4,7 @@ namespace SkillsHunterAPI.Models.Project
     public class Application
     {
         public Guid ApplicationId { get; set; }
+        public Guid AppicantId { get; set; }
+        public Guid ProjectId { get; set; }
     }
 }
