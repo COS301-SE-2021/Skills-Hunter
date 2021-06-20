6e3478f335b7e12470d4ad4bd47a2d0c99b52f75
Tshepang <tshepangmog10@gmail.com>
2021-06-21 00:25:32 +0200

    Added filter button

--- SkillsHunterFrontend/src/app/admin-portal/admin-portal.component.ts
+++ SkillsHunterFrontend/src/app/admin-portal/admin-portal.component.ts
@@ -1,6 +1,6 @@
 import { Skills } from './../mock-data/mock-skills';
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { MatTable } from '@angular/material/table';
+import { MatTable, MatTableDataSource } from '@angular/material/table';
 import { Skill } from '../classes/Skill';
 import {
   animate,
@@ -33,17 +33,29 @@ export class AdminPortalComponent implements OnInit {
   columnsToDisplay = ['name'];
   dataSource = Skills;
   expandedElement: Skill | null;
+  dataSourceTB = new MatTableDataSource(Skills);
 
   @ViewChild(MatTable) table: MatTable<Skill>;
 
   addData() {
     const randomElementIndex = Math.floor(Math.random() * Skills.length);
     this.dataSource.push(Skills[randomElementIndex]);
+    // refresh the list of skills:
+    this.dataSourceTB = new MatTableDataSource(Skills);
     this.table.renderRows();
   }
 
   removeData() {
     this.dataSource.pop();
+    // refresh the list of skills:
+    this.dataSourceTB = new MatTableDataSource(Skills);
     this.table.renderRows();
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    // refresh the list of skills:
+    this.dataSourceTB = new MatTableDataSource(Skills);
+    this.dataSourceTB.filter = filterValue.trim().toLowerCase();
   }
 }
