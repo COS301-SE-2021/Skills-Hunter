c5d834f3d89cc714cc3668d97c7236f502abd838
Tshepang <tshepangmog10@gmail.com>
2021-06-21 00:01:02 +0200

    Modified skill table template

--- SkillsHunterFrontend/src/app/admin-portal/admin-portal.component.ts
+++ SkillsHunterFrontend/src/app/admin-portal/admin-portal.component.ts
@@ -2,19 +2,37 @@ import { Skills } from './../mock-data/mock-skills';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatTable } from '@angular/material/table';
 import { Skill } from '../classes/Skill';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-admin-portal',
   templateUrl: './admin-portal.component.html',
   styleUrls: ['./admin-portal.component.scss'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({ height: '0px', minHeight: '0' })),
+      state('expanded', style({ height: '*' })),
+      transition(
+        'expanded <=> collapsed',
+        animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')
+      ),
+    ]),
+  ],
 })
 export class AdminPortalComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
 
-  displayedColumns: string[] = ['name'];
+  columnsToDisplay = ['name'];
   dataSource = Skills;
+  expandedElement: Skill | null;
 
   @ViewChild(MatTable) table: MatTable<Skill>;
 
