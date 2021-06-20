cea9e1225d8d9a0e4b85ca9b967ef584f9743a0b
Tshepang <tshepangmog10@gmail.com>
2021-06-20 23:32:41 +0200

    Added skill card template

--- SkillsHunterFrontend/src/app/admin-portal/admin-portal-card/admin-portal-card.component.ts
+++ SkillsHunterFrontend/src/app/admin-portal/admin-portal-card/admin-portal-card.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Skill } from 'src/app/classes/Skill';
 
 @Component({
   selector: 'app-admin-portal-card',
   templateUrl: './admin-portal-card.component.html',
-  styleUrls: ['./admin-portal-card.component.scss']
+  styleUrls: ['./admin-portal-card.component.scss'],
 })
 export class AdminPortalCardComponent implements OnInit {
+  @Input() card_skill: Skill;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
