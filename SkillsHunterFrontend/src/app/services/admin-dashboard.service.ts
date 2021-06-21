f566445078becf3c7f02ed86eb96a08ebf44376f
Tshepang <tshepangmog10@gmail.com>
2021-06-21 14:01:07 +0200

    Created admin add skill request

--- SkillsHunterFrontend/src/app/services/admin-dashboard.service.ts
+++ SkillsHunterFrontend/src/app/services/admin-dashboard.service.ts
@@ -1,9 +1,23 @@
+import { Skill } from 'src/app/classes/Skill';
+import { AdminAddSkillComponent } from './../admin-portal/admin-add-skill/admin-add-skill.component';
+import { AdminPortalComponent } from './../admin-portal/admin-portal.component';
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminDashboardService {
 
-  constructor() { }
+  constructor(private httpclient: HttpClient) { }
+
+//send request to back end to validate user login details
+adminAddSkill(formData: Skill): Observable<any> {
+  return this.httpclient.post(
+    'http://localhost:5000/api/Admin/addSkill',
+    formData
+  );
+}
+
 }
