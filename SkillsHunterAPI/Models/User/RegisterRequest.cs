422b0b5931de1d9e214a5bb3dea8f0a62557d8b3
Joseph-Makgopa <u18166793@tuks.co.za>
2021-06-24 21:55:28 +0200

    added required annotations

--- SkillsHunterAPI/Models/User/RegisterRequest.cs
+++ SkillsHunterAPI/Models/User/RegisterRequest.cs
@@ -1,15 +1,26 @@
-using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace SkillsHunterAPI.Models.User
 {
     //This class will be used to contain all the User data that is sent through with a Project creation or update request
     public class RegisterRequest
     {
+        [Required]
         public string Name {get; set; }
+        
+        [Required]
         public string Surname {get;set;}
+        
+        [Required]
         public string Phone { get; set; }
+        
+        [Required]
         public string Email {get; set;}
+        
+        [Required]
         public string Password {get; set;}
+        
+        [Required]
         public UserType Role {get; set;}
     }
 }
