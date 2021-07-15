38c3a3667f0d90dad2dceae9ada0c479b2f48b68
MxolisiMkanzi <u18059326@tuks.co.za>
2021-07-15 17:19:37 +0200

    Change Collection name to Skill collection

--- SkillsHunterAPI/Models/Skill/Entity/SkillCollectionMap.cs
+++ SkillsHunterAPI/Models/Skill/Entity/SkillCollectionMap.cs
@@ -3,10 +3,10 @@ using System;
 namespace SkillsHunterAPI.Models.Skill
 {   
     //This model entity class contains attributes that maps a skill to a collection
-    public class CollectionMap
+    public class SkillCollectionMap
     {
-        public Guid CollectionMapId { get; set; }
-        public Guid CollectionId { get; set; }
+        public Guid SkillCollectionMapId { get; set; }
+        public Guid SkillCollectionId { get; set; }
         public Guid SkillId { get; set; }
     }
 }
