85c94728d0d4dd4d8687e737c7e5a00e74c42952
MxolisiMkanzi <u18059326@tuks.co.za>
2021-07-15 18:20:04 +0200

    Modified the AddSkillRequest class
    It now accommodates the new one-to-many relationship between skills and categories

--- SkillsHunterAPI/Models/Skill/Request/AddSkillRequest.cs
+++ SkillsHunterAPI/Models/Skill/Request/AddSkillRequest.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace SkillsHunterAPI.Models.Skill
 {
@@ -6,6 +7,6 @@ namespace SkillsHunterAPI.Models.Skill
     public class AddSkillRequest
     {
         public String Name { get; set; }
-        public Guid CategoryId { get; set; }
+        public List<Guid> Categories { get; set; }
     }
 }
