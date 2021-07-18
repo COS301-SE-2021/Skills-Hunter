41f3d0927b1aa1fe1512d770d01d116ab435cba5
MxolisiMkanzi <u18059326@tuks.co.za>
2021-07-18 18:38:54 +0200

    Updated the structure of the AddSkillCollection request to accommodate the Description field.

--- SkillsHunterAPI/Models/Skill/Request/AddSkillCollectionRequest.cs
+++ SkillsHunterAPI/Models/Skill/Request/AddSkillCollectionRequest.cs
@@ -6,7 +6,7 @@ namespace SkillsHunterAPI.Models.Skill.Request
     public class AddSkillCollectionRequest
     {
         public string Name { get; set; }
-
+        public string Description { get; set; }
         public List<GetSkillByIdRequest> Skills { get; set; }
     }
 }
