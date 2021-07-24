c849a4482d98813426b56ec4ee697f3d1e9097aa
MxolisiMkanzi <u18059326@tuks.co.za>
2021-07-24 18:01:29 +0200

    Added the weight field on some classes

--- SkillsHunterAPI/Models/Project/Request/AddSkillCollectionRequest.cs
+++ SkillsHunterAPI/Models/Project/Request/AddSkillCollectionRequest.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace SkillsHunterAPI.Models.Skill.Request
+namespace SkillsHunterAPI.Models.Project.Request
 {
     public class AddSkillCollectionRequest
     {
         public string Name { get; set; }
         public string Description { get; set; }
-        public List<GetSkillByIdRequest> Skills { get; set; }
+        public int Weight { get; set; }
+        public List<AddExistingSkillRequest> Skills { get; set; }
     }
 }
--- SkillsHunterAPI/Models/Project/Request/CreateCollectionRequest.cs
+++ SkillsHunterAPI/Models/Project/Request/CreateCollectionRequest.cs
@@ -1,6 +1,6 @@
 using System;
 
-namespace SkillsHunterAPI.Models.Skill
+namespace SkillsHunterAPI.Models.Project.Request
 {
     //This model request class contains attributes required to create collection in the system
     public class CreateCollectionRequest
