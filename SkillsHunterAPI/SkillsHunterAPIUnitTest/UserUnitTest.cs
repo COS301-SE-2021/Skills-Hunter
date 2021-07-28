04b38cd8d49452a4003d36ab297a835029d81b91
MxolisiMkanzi <u18059326@tuks.co.za>
2021-07-28 16:48:23 +0200

    Defined all the user functions to be tested in the userTest class

---
+++ SkillsHunterAPI/SkillsHunterAPIUnitTest/UserUnitTest.cs
@@ -0,0 +1,66 @@
+using System;
+using FakeItEasy;
+using SkillsHunterAPI.Controllers;
+using SkillsHunterAPI.Models.Skill;
+using SkillsHunterAPI.Services;
+using Xunit;
+
+namespace SkillsHunterAPIUnitTest.Tests
+{
+    public class UserUnitTest : SkillsHunterAPIUnitTestBase
+    {
+
+        public UserUnitTest()
+        {
+
+        }
+
+        [Fact]
+        public void TestRegister()
+        {
+
+        }
+
+        [Fact]
+        public void TestAuthenticate()
+        {
+
+        }
+
+        [Fact]
+        public void TestGetCurrentUserId()
+        {
+
+        }
+
+        [Fact]
+        public void TestLogout()
+        {
+
+        }
+
+        [Fact]
+        public void TestUpdateUser()
+        {
+
+        }
+
+        [Fact]
+        public void TestDeleteUser()
+        {
+
+        }
+
+        [Fact]
+        public void TestGetAllUsers()
+        {
+
+        }
+
+        [Fact]
+        public void TestGetUser()
+        {
+
+        }
+    }
+}
