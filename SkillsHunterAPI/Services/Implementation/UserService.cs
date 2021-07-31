a29c590d89c2b5f8b9d3d8ae8bf01bbd9ce29f7f
MxolisiMkanzi <u18059326@tuks.co.za>
2021-07-31 19:40:27 +0200

    Added the functions to add user skills and collections on the implementation

--- SkillsHunterAPI/Services/Implementation/UserService.cs
+++ SkillsHunterAPI/Services/Implementation/UserService.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Cryptography.KeyDerivation;
 using System.Collections.Generic;
 using SkillsHunterAPI.Data;
 using System.Linq;
+using SkillsHunterAPI.Models.User.Request;
 
 namespace SkillsHunterAPI.Services
 {
@@ -260,5 +261,21 @@ namespace SkillsHunterAPI.Services
 
             return result;
         }
+
+        public Task AddUserSkill(AddUserSkillRequest request)
+        {
+            return null;
+        }
+
+        public Task AddNewSkill(AddNewSkillRequest request)
+        {
+            return null;
+        }
+
+        public Task AddUserSkillCollection(AddUserSkillCollectionRequest request)
+        {
+            return null;
+        }
+
     }
 }
