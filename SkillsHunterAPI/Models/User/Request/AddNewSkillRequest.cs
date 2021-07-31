a7c315410cee88b35fb3be1f85af42b76f28b62b
MxolisiMkanzi <u18059326@tuks.co.za>
2021-07-31 19:38:24 +0200

    Added the request class for adding new skills (by the user) and added it's fields

---
+++ SkillsHunterAPI/Models/User/Request/AddNewSkillRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using SkillsHunterAPI.Models.Skill.Request;
+
+namespace SkillsHunterAPI.Models.User.Request
+{
+    public class AddNewSkillRequest
+    {
+        public String Name { get; set; }
+        public List<GetCategoryByIdRequest> Categories { get; set; }
+        public int Weight { get; set; }
+
+        public AddNewSkillRequest()
+        {
+        }
+    }
+}
