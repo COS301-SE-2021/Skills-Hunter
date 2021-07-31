626f2ac81e5ff5cdf904cce45f3bfd437c065947
MxolisiMkanzi <u18059326@tuks.co.za>
2021-07-31 19:39:31 +0200

    Added the request class for adding user skills and added the required fields

---
+++ SkillsHunterAPI/Models/User/Request/AddUserSkillRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace SkillsHunterAPI.Models.User.Request
+{
+    public class AddUserSkillRequest
+    {
+        public String Name { get; set; }
+        public int Weight { get; set; }
+        public AddUserSkillRequest()
+        {
+        }
+    }
+}
