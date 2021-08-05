d8aaf71c2daded816d6374f727e9c6a6b7f4da86
MxolisiMkanzi <u18059326@tuks.co.za>
2021-08-05 20:34:13 +0200

    Added a Test project for the user subsystem

---
+++ SkillsHunterAPI/SkillsHunterUserUnitTest/UserUnitTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xunit;
+
+namespace SkillsHunterUserUnitTest
+{
+    public class UserUnitTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
