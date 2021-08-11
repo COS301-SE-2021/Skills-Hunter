83c461c94f60ae60982d485090149ea835938a54
Sboniso Masilela <u10416260@tuks.co.za>
2021-08-11 23:01:00 +0200

    Updated code on UserSkillCollection

--- SkillsHunterAPI/Models/User/Entity/UserSkillCollection.cs
+++ SkillsHunterAPI/Models/User/Entity/UserSkillCollection.cs
@@ -4,10 +4,10 @@ namespace SkillsHunterAPI.Models.User.Entity
     public class UserSkillCollection
     {
         public Guid UserSkillCollectionId { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
         public int Weight { get; set; }
         public Guid UserId { get; set; }
+
+        public Guid SkillCollectionId { get; set; }
 
         public UserSkillCollection()
         {
