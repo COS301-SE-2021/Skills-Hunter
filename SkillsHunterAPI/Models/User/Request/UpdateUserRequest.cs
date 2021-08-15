254df293052c980ff050d5ab46690974e834717e
Sboniso Masilela <u10416260@tuks.co.za>
2021-08-15 20:23:32 +0200

    Updated UpdateUserRequest

--- SkillsHunterAPI/Models/User/Request/UpdateUserRequest.cs
+++ SkillsHunterAPI/Models/User/Request/UpdateUserRequest.cs
@@ -7,7 +7,7 @@ namespace SkillsHunterAPI.Models.User
     public class UpdateUserRequest
     {
 
-        public Guid UserId { get; set; }
+        //public Guid UserId { get; set; }
         public string Name { get; set; }
         public string Surname {get;set; }
         public string Email {get; set; }
