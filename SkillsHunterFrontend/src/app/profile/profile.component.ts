52dc6e5e586d39baf7b6fb4e4dfaaa4886862704
Thuso-Khoza <u17088799@tuks.co.za>
2021-08-15 13:32:04 -0700

    profile ts file update

--- SkillsHunterFrontend/src/app/profile/profile.component.ts
+++ SkillsHunterFrontend/src/app/profile/profile.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ProfileInfoService } from '../services/profile-info.service';
+import { MatDialogRef,MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { AddSkillComponent } from './add-skill/add-skill.component';
+import { ProfileImgComponent } from './profile-img/profile-img.component';
 
 @Component({
   selector: 'app-profile',
@@ -12,18 +15,18 @@ export class ProfileComponent implements OnInit {
   imageUrl: string = "/assets/images/profile.png";
   fileToUpload: File = null;
 
-  constructor(private imgService: ProfileInfoService) { }
+  constructor(private imgService: ProfileInfoService,private dialog: MatDialog) { }
 
   ngOnInit(): void {
-    localStorage.setItem('name',data.body.name);
-    localStorage.setItem('surname',data.body.surname);
-    localStorage.setItem('name',data.body.name);
-    localStorage.setItem('surname',data.body.surname);
+    localStorage.setItem('name','Emmanuel');
+    localStorage.setItem('surname','Khoza');
+    localStorage.setItem('email','emma@mail.com');
+    localStorage.setItem('phone','0630000000');
 
-    this.personalDetailsForm.controls['name'].setValue(this.getProjectInfo.name);
-    this.personalDetailsForm.controls['name'].setValue(this.getProjectInfo.name);
-    this.personalDetailsForm.controls['name'].setValue(this.getProjectInfo.name);
-    this.personalDetailsForm.controls['name'].setValue(this.getProjectInfo.name);
+    this.personalDetailsForm.controls['name'].setValue(localStorage.getItem('name'));
+    this.personalDetailsForm.controls['surname'].setValue(localStorage.getItem('surname'));
+    this.personalDetailsForm.controls['email'].setValue(localStorage.getItem('email'));
+    this.personalDetailsForm.controls['phone'].setValue(localStorage.getItem('phone'));
 
   }
 
@@ -38,6 +41,7 @@ export class ProfileComponent implements OnInit {
     ]),
     role: new FormControl('', [Validators.required]),
     email: new FormControl('', [Validators.required, Validators.email]),
+    phone: new FormControl('', [Validators.required,Validators.pattern('[- +()0-9]+')]),
     password: new FormControl('', [Validators.required]),
   });
 
@@ -62,5 +66,48 @@ export class ProfileComponent implements OnInit {
        this.imageUrl = "/assets/img/default-image.png";
      }
    );
+  }
+
+  viewImg(){
+ 
+    const configDialog = new MatDialogConfig();
+    const dialogRef = this.dialog.open(ProfileImgComponent,
+      {   width: '40%',
+         height:'80%',
+        data: { img: this.imageUrl}
+      });
+   // console.log("back");
+    dialogRef.afterClosed().subscribe(data => {
+   
+      if(data !=undefined)
+      {
+        this.imageUrl=data.img;
+      //return the skill and the value to profile then send request to backend
+      }
+      else{ 
+        console.log("returned empty:");
+      }//dialog closed
+    });
+  }
+
+  addskill(){
+    const configDialog = new MatDialogConfig();
+    const dialogRef = this.dialog.open(AddSkillComponent,
+      {   width: '40%',
+         height:'80%'
+      });
+   // console.log("back");
+    dialogRef.afterClosed().subscribe(returnedData => {
+   
+      if(returnedData !=undefined)
+      {
+        console.log("profile: "+returnedData.data.selectedSkill);
+        console.log("rate: "+returnedData.data.datarateValue);
+      //return the skill and the value to profile then send request to backend
+      }
+      else{ 
+        console.log("returned empty:");
+      }//dialog closed
+    });
   }
 }
