314397d592bd5bc14a3b4f4f876de6b2b045ae2a
MxolisiMkanzi <u18059326@tuks.co.za>
2021-08-17 21:23:44 +0200

    Added a get project skill response class

--- SkillsHunterAPI/Models/Project/Response/GetProjectSkillCollectionResponse.cs
+++ SkillsHunterAPI/Models/Project/Response/GetProjectSkillCollectionResponse.cs
@@ -1,14 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using SkillsHunterAPI.Models.Project.Response;
 
 namespace SkillsHunterAPI.Models.Project.Request
 {
     public class GetProjectSkillCollectionResponse
     {
+        Guid ProjectSkillCollectionId { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public int Weight { get; set; }
-        public List<AddExistingSkillRequest> Skills { get; set; }
+        public List<GetProjectSkillResponse> Skills { get; set; }
 
         public GetProjectSkillCollectionResponse()
         {
---
+++ SkillsHunterAPI/Models/Project/Response/GetProjectSkillResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace SkillsHunterAPI.Models.Project.Response
+{
+    public class GetProjectSkillResponse
+    {
+        public Guid SkillId { get; set; }
+        public string Name { get; set; }
+        public int Weight { get; set; }
+
+        public GetProjectSkillResponse()
+        {
+        }
+    }
+}
