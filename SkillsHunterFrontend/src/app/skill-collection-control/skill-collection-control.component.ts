8a42fedd3750a7afa60c54cea24f3abaf2ced70c
Joseph-Makgopa <u18166793@tuks.co.za>
2021-08-18 23:15:51 +0200

    project service implementation

--- SkillsHunterFrontend/src/app/skill-collection-control/skill-collection-control.component.ts
+++ SkillsHunterFrontend/src/app/skill-collection-control/skill-collection-control.component.ts
@@ -28,20 +28,6 @@ export class SkillCollectionControlComponent implements OnInit {
 
   }
 
-  deleteCollection(collection): void{
-    // for(var count = 0; count < this.data.length;count++){
-    //   if(this.data[count].ProjectSkillCollectionId == collection.ProjectSkillCollectionId){
-    //     for(var step = count; step < this.data.length - 1; step++){
-    //       this.data[step] = this.data[step + 1];
-    //     }
-    //     this.data.pop();
-    //     break;
-    //   }
-    // }
-
-    // this.ngOnInit();    
-  }
-
   advancedSearch(): void {
     // const configDialog = new MatDialogConfig();
     // configDialog.backdropClass = 'backGround';
@@ -52,48 +38,51 @@ export class SkillCollectionControlComponent implements OnInit {
     // const dialogRef = this.dialog.open(SkillCollectionAdvancedSearchComponent,configDialog);
 
     // dialogRef.afterClosed().subscribe(result => {
-    //   this.adminService.getSkillCollections().subscribe(result=>{
-    //     this.data = result;
+    //   this.adminService.getSkillCollections().subscribe(response=>{
+    //     this.data = response;
+    //     let newList: SkillCollection[] = new Array();
+        
+    //     if(result == undefined || result == "#"){
+    //       return;
+    //     }
+        
+    //     for(let count = 0; count < this.data.length; count++){
+    //       if(this.data[count].ProjectId == result)
+    //         newList.push(this.data[count]);
+    //     }
+  
+    //     this.data = newList;
     //     this.ngOnInit();
     //   })
-    //   this.data = mockSkillCollection;
-    //   let newList: SkillCollection[] = new Array();
       
-    //   if(result == undefined || result == "#"){
-    //     return;
-    //   }
-      
-    //   for(let count = 0; count < this.data.length; count++){
-    //     if(this.data[count].ProjectId == result)
-    //       newList.push(this.data[count]);
-    //   }
 
-    //   this.data = newList;
-    //   this.ngOnInit();
     // });
-  }
+  } 
 
   Search(): void{
-    // if(this.searchTerm != ""){
-    //   let tempData:SkillCollection[] = mockSkillCollection;
-    //   let result: SkillCollection = null;
-      
-    //   for(let count  = 0; count < tempData.length; count++){
-    //     if(tempData[count].Name == this.searchTerm){
-    //       result = tempData[count];
-    //       break;
-    //     }
-    //   }
+    if(this.searchTerm != ""){
+      this.adminService.getSkillCollections().subscribe(response=>{
+        let tempData:getSkillCollectionResponse[] = response;
+        let result: getSkillCollectionResponse = null;
+        
+        for(let count  = 0; count < tempData.length; count++){
+          if(tempData[count].Name == this.searchTerm){
+            result = tempData[count];
+            break;
+          }
+        }
+  
+        if(result != null){
+          this.data = [];
+          this.data.push(result);
+          this.ngOnInit();
+        }else{
+          this.data = [];
+          this.ngOnInit();
+        }
+      })
 
-    //   if(result != null){
-    //     this.data = [];
-    //     this.data.push(result);
-    //     this.ngOnInit();
-    //   }else{
-    //     this.data = [];
-    //     this.ngOnInit();
-    //   }
-    // }    
+    }    
   }
   
 }
