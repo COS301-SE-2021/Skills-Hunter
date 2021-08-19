402533ac5c7ffa4f1ae0eeaebae066455a841b81
MxolisiMkanzi <u18059326@tuks.co.za>
2021-08-19 08:04:03 +0200

    Added a response class for retrieving user skills

---
+++ SkillsHunterAPI/Models/User/Response/GetUserSkillResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace SkillsHunterAPI.Models.User.Response
+{
+    public class GetUserSkillResponse
+    {
+        public Guid skillId { get; set; }
+        public Guid Name { get; set; }
+        public int Weight { get; set; }
+        public GetUserSkillResponse()
+        {
+        }
+    }
+}
