641455b6ebbb6a6b9ab7d95ea674ad4a055d867b
Tshepang <tshepangmog10@gmail.com>
2021-08-19 12:53:25 +0200

    More uncommented

--- SkillsHunterFrontend/src/app/createproject/createproject.component.ts
+++ SkillsHunterFrontend/src/app/createproject/createproject.component.ts
@@ -18,6 +18,8 @@ import { Skills } from '../mock-data/mock-skills';
 import { IDropdownSettings } from 'ng-multiselect-dropdown';
 import { Skill } from '../classes/Skill';
 import { mockSkillCollection } from '../mock-data/mock-collection';
+import { AddSkillCategoryComponent } from './add-skill-category/add-skill-category.component';
+import { AddSkillCollectionComponent } from './add-skill-collection/add-skill-collection.component';
 
 @Component({
   selector: 'app-createproject',
@@ -140,11 +142,7 @@ export class CreateprojectComponent implements OnInit {
     // NEITHER = -1,-1
 
     if (occurance[0] == 0) {
-      // process skill:
-      // var obj = new Skill();
-      // obj.SkillId = Skills[occurance[1]].SkillId;
-      // obj.SkillName = Skills[occurance[1]].SkillName;
-      // obj.SkillWeight = this.skillWeight;
+
       var obj = {
         SkillId: Skills[occurance[1]].SkillId,
         Name: Skills[occurance[1]].Name,
@@ -176,24 +174,24 @@ export class CreateprojectComponent implements OnInit {
     configDialog.backdropClass = 'backGround';
     configDialog.width = '45%';
     configDialog.height = '450px';
-    // const dialogRef = this.dialog.open(AddSkillCategoryComponent, configDialog);
+    const dialogRef = this.dialog.open(AddSkillCategoryComponent, configDialog);
 
-    // dialogRef.afterClosed().subscribe((skill) => {
-    //   if (skill != undefined) {
-    //     var newSkill = {
-    //       name: skill.data.name,
-    //       categories: skill.data.categories,
-    //       weight: skill.data.weight,
-    //     };
+    dialogRef.afterClosed().subscribe((skill) => {
+      if (skill != undefined) {
+        var newSkill = {
+          name: skill.data.name,
+          categories: skill.data.categories,
+          weight: skill.data.weight,
+        };
 
-    //     this.newSkillsArray.push(newSkill);
+        this.newSkillsArray.push(newSkill);
 
-    //     console.log('Added the Skills.');
-    //     this.dropdownListForSkills = [];
+        console.log('Added the Skills.');
+        this.dropdownListForSkills = [];
 
-    //     this.ngOnInit();
-    //   } else console.log('Returned Empty Skill');
-    // });
+        this.ngOnInit();
+      } else console.log('Returned Empty Skill');
+    });
   }
 
   addCollection() {
@@ -201,29 +199,29 @@ export class CreateprojectComponent implements OnInit {
     configDialog.backdropClass = 'backGround';
     configDialog.width = '60%';
     configDialog.height = '450px';
-    // const dialogRef = this.dialog.open(
-    //   AddSkillCollectionComponent,
-    //   configDialog
-    // );
+    const dialogRef = this.dialog.open(
+      AddSkillCollectionComponent,
+      configDialog
+    );
 
 
-    // dialogRef.afterClosed().subscribe((collection) => {
-    //   if (collection != undefined) {
-    //     var newCollection = {
-    //       name: collection.data.name,
-    //       description: collection.data.description,
-    //       weight: collection.data.weight,
-    //       skills: collection.data.skills,
-    //     };
+    dialogRef.afterClosed().subscribe((collection) => {
+      if (collection != undefined) {
+        var newCollection = {
+          name: collection.data.name,
+          description: collection.data.description,
+          weight: collection.data.weight,
+          skills: collection.data.skills,
+        };
 
-    //     this.collectionOfSkills.push(newCollection);
+        this.collectionOfSkills.push(newCollection);
 
-    //     console.log('Added the Collection.');
-    //     this.dropdownListForCollections = [];
+        console.log('Added the Collection.');
+        this.dropdownListForCollections = [];
 
-    //     this.ngOnInit();
-    //   } else console.log('Returned Empty Collection');
-    // });
+        this.ngOnInit();
+      } else console.log('Returned Empty Collection');
+    });
   }
 
   createTheProject() {
