1d3dcc99f925396a5cd2ee72e2b1ef3de929a9c9
MxolisiMkanzi <u18059326@tuks.co.za>
2021-09-14 01:20:10 +0200

    Create GetUserSkillResponse.cs

---
+++ SkillsHunterAPI/Models/User/Request/GetUserSkillResponse.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace SkillsHunterAPI.Models.User.Request
+{
+    public class GetUserSkillResponse
+    {
+        public GetUserSkillResponse()
+        {
+        }
+    }
+}
