3a790e22dc84c8261b2573961605a9d6c97419ce
MxolisiMkanzi <u18059326@tuks.co.za>
2021-09-14 01:19:09 +0200

    Created a MatchSkill class that will contain the matching user skills

---
+++ SkillsHunterAPI/Models/Project/Entity/MatchingSkill.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace SkillsHunterAPI.Models.Project.Entity
+{
+    public class MatchingSkill
+    {
+        public Guid SkillId { get; set; }
+        public String Name { get; set; }
+        public double Percentage { get; set; }
+
+        public MatchingSkill()
+        {
+        }
+    }
+}
