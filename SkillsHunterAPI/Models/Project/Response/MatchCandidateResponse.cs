3cf74a3632c122b45317472e655055861b36f035
MxolisiMkanzi <u18059326@tuks.co.za>
2021-09-14 01:19:47 +0200

    Created a MatchCandidateResponse that will hold the matching candidates

---
+++ SkillsHunterAPI/Models/Project/Response/MatchCandidateResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using SkillsHunterAPI.Models.Project.Entity;
+
+namespace SkillsHunterAPI.Models.Project.Response
+{
+    public class MatchCandidateResponse
+    {
+        public Guid UserId { get; set; }
+        public String Name { get; set; }
+        public double Percentage { get; set; }
+        public List<MatchingSkill> MatchingSkills { get; set; }
+
+        public MatchCandidateResponse()
+        {
+            MatchingSkills = new List<MatchingSkill>();
+        }
+    }
+}
