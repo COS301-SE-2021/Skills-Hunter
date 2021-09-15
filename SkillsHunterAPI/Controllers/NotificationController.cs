91ba83fb6446f49a77fec7866fb5e243edaaba24
Sboniso Masilela <u10416260@tuks.co.za>
2021-09-15 13:44:17 +0200

    Added message class and attributes & SendToSpecificUser method on controller

--- SkillsHunterAPI/Controllers/NotificationController.cs
+++ SkillsHunterAPI/Controllers/NotificationController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SignalR;
 using SkillsHunterAPI.Hubs;
+using SkillsHunterAPI.Models.Notification;
 using SkillsHunterAPI.Services.Interface;
 using System;
 using System.Collections.Generic;
@@ -32,7 +33,27 @@ namespace SkillsHunterAPI.Controllers
         }
 
 
+        [HttpPost]
+        public async Task<ActionResult> SendToSpecificUser(Message model)
+        {
+            var currentLoggedInUser = ' ';
 
+            /*var connections = _userConnectionManager.GetUserConnections(model.);
+            if (connections != null && connections.Count > 0)
+            {
+                foreach (var connectionId in connections)
+                {
+                    await _notificationUserHubContext.Clients.Client(connectionId).SendAsync("sendToUser", model.articleHeading, model.articleContent);
+                }
+            }
+            return View();*/
 
+            return null;
+        }
     }
+
+
+
+
+}
 }
---
+++ SkillsHunterAPI/Models/Notification/Message.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SkillsHunterAPI.Models.Notification
+{
+    public class Message
+    {
+        public string message { get; set; }
+
+        public bool isRead { get; set; }
+
+        public DateTime created { get; set; } = DateTime.Now;
+    }
+}
