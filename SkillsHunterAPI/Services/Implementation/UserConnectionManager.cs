c837d3b9f94c0f0a2c0c300b1bca4d01c02862f3
Sboniso Masilela <u10416260@tuks.co.za>
2021-09-15 12:14:00 +0200

    defined and implemented KeepUserConnection method

---
+++ SkillsHunterAPI/Services/Implementation/UserConnectionManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SkillsHunterAPI.Services.Implementation
+{
+    public class UserConnectionManager
+    {
+
+        private static Dictionary<string, List<string>> userConnectionMap = new Dictionary<string, List<string>>();
+        private static string userConnectionMapLocker = string.Empty;
+
+        public void KeepUserConnection(string userId, string connectionId)
+        {
+            lock (userConnectionMapLocker)
+            {
+                if (!userConnectionMap.ContainsKey(userId))
+                {
+                    userConnectionMap[userId] = new List<string>();
+                }
+                userConnectionMap[userId].Add(connectionId);
+            }
+        }
+
+    }
+}
