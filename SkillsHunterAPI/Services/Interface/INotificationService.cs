df07d60bdf684d37e835ed683edaa7a010d428b8
Sboniso Masilela <u10416260@tuks.co.za>
2021-09-16 00:04:55 +0200

    Created Notification Interface with functions

---
+++ SkillsHunterAPI/Services/Interface/INotificationService.cs
@@ -0,0 +1,23 @@
+﻿using SkillsHunterAPI.Models.Notification;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SkillsHunterAPI.Services.Interface
+{
+    public interface INotificationService
+    {
+
+
+        Task<IEnumerable<Notification>> GetNotificationsByOwnerId();
+
+        Task<IEnumerable<Notification>> GetUnReadNotificationsByOwnerId();
+
+        Task UpdatingReadStatus(Guid notificationId);
+
+        Task<Notification> SendNotifications(Notification notification);
+
+        Task DeleteNotifications(Guid id);
+    }
+}
