0b2225f17f2a9467a928723299da8ecc783b0739
Sboniso Masilela <u10416260@tuks.co.za>
2021-09-16 00:49:23 +0200

    Defined all the Notification service methods

--- SkillsHunterAPI/Services/Implementation/NotificationService.cs
+++ SkillsHunterAPI/Services/Implementation/NotificationService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SkillsHunterAPI.Models.Notification;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,29 @@ namespace SkillsHunterAPI.Services.Implementation
 {
     public class NotificationService
     {
+        Task<IEnumerable<Notification>> GetNotificationsByOwnerId()
+        {
+            return null;
+        }
+
+        Task<IEnumerable<Notification>> GetUnReadNotificationsByOwnerId()
+        {
+            return null;
+        }
+
+        Task UpdatingReadStatus(Guid notificationId)
+        {
+            return null;
+        }
+
+        Task<Notification> SendNotifications(Notification notification)
+        {
+            return null;
+        }
+
+        Task DeleteNotifications(Guid id)
+        {
+            return null;
+        }
     }
 }
