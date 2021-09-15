ed2975e62a7a86dfd77b5b2d000ba41d8724f49f
Sboniso Masilela <u10416260@tuks.co.za>
2021-09-15 11:55:27 +0200

    Defined and implemented OnDisconnectedAsync function

--- SkillsHunterAPI/Hubs/NotificationUserHub.cs
+++ SkillsHunterAPI/Hubs/NotificationUserHub.cs
@@ -9,8 +9,10 @@ namespace SkillsHunterAPI.Hubs
 {
     public class NotificationUserHub:Hub
     {
-
+        
+        //defines the IUserConnectionManager object
         private readonly IUserConnectionManager _userConnectionManager;
+
         public NotificationUserHub(IUserConnectionManager userConnectionManager)
         {
             _userConnectionManager = userConnectionManager;
@@ -18,12 +20,28 @@ namespace SkillsHunterAPI.Hubs
 
         public string GetConnectionId()
         {
+            
             var httpContext = this.Context.GetHttpContext();
+
             var userId = httpContext.Request.Query["userId"];
+
             _userConnectionManager.KeepUserConnection(userId, Context.ConnectionId);
 
             return Context.ConnectionId;
         }
 
+        //This function is called when a connection in the hub is terminated/closed.
+        public async override Task OnDisconnectedAsync(Exception exception)
+        {
+            //get the connectionId
+            var connectionId = Context.ConnectionId;
+
+            _userConnectionManager.RemoveUserConnection(connectionId);
+
+            //adding dump code to follow the template of Hub > OnDisconnectedAsync
+            var value = await Task.FromResult(0);
+        }
     }
+
 }
+
