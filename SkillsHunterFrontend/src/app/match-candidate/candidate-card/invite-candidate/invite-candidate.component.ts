d8d70c2199beda3b454bc896fc9ba73ac61df3d2
Tshepang <u17152837@tuks.co.za>
2021-09-16 12:55:50 +0200

    implementation for Invite

--- SkillsHunterFrontend/src/app/match-candidate/candidate-card/invite-candidate/invite-candidate.component.ts
+++ SkillsHunterFrontend/src/app/match-candidate/candidate-card/invite-candidate/invite-candidate.component.ts
@@ -6,48 +6,40 @@ import { MatDialogRef } from '@angular/material/dialog';
 @Component({
   selector: 'app-invite-candidate',
   templateUrl: './invite-candidate.component.html',
-  styleUrls: ['./invite-candidate.component.scss']
+  styleUrls: ['./invite-candidate.component.scss'],
 })
 export class InviteCandidateComponent implements OnInit {
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
-  constructor(public dialogRef: MatDialogRef<InviteCandidateComponent>, private projService: ProjectCRUDService) {}
+  constructor(
+    public dialogRef: MatDialogRef<InviteCandidateComponent>,
+    private projService: ProjectCRUDService
+  ) {}
 
   invitationForm: FormGroup = new FormGroup({
-    candidateMessage: new FormControl('', [Validators.required])
+    candidateMessage: new FormControl('', [Validators.required]),
   });
 
   onSubmit() {
-
-
     var formData = {
-      "userId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-      "projectId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-      "inviteeId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-      "message": "message is this!"
-    }
+      userId: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
+      projectId: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
+      inviteeId: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
+      message: 'message is this!',
+    };
 
-    // formData.message = <string>(
-    //   (<any>this.invitationForm.controls['candidateMessage'].value)
-    // );
-    
-    console.log("MESSAGE IS = "+formData.message);
-    
+    console.log('MESSAGE IS = ' + formData.message);
+
     this.projService.inviteCandidate(formData).subscribe((data) => {
       console.log('Candidate was invited...');
       console.log('Response: ', data);
     });
 
-    this.dialogRef.close();
-    // this.cancel();
+    this.cancel();
   }
 
   //close dialog popup
   cancel() {
     this.dialogRef.close();
   }
-
-
 }
