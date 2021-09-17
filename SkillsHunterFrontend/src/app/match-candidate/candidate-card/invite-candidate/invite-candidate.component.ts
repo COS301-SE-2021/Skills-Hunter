a97d72f80e882fad11c2d0bec95f679a43d889ae
Tshepang <u17152837@tuks.co.za>
2021-09-17 11:52:14 +0200

    Completed Invitation functionality

--- SkillsHunterFrontend/src/app/match-candidate/candidate-card/invite-candidate/invite-candidate.component.ts
+++ SkillsHunterFrontend/src/app/match-candidate/candidate-card/invite-candidate/invite-candidate.component.ts
@@ -1,6 +1,11 @@
 import { ProjectCRUDService } from './../../../services/project-crud.service';
 import { Component, Inject, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder,
+} from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
@@ -10,9 +15,14 @@ import { MatSnackBar } from '@angular/material/snack-bar';
   styleUrls: ['./invite-candidate.component.scss'],
 })
 export class InviteCandidateComponent implements OnInit {
+  invitationFormGroup: FormGroup;
   userID: any;
 
   ngOnInit(): void {
+    this.invitationFormGroup = this.formBuilder.group({
+      invitationMessage: ['', Validators.required],
+    });
+
     this.projService.getMyUserID().subscribe((uID) => {
       this.userID = uID;
     });
@@ -22,36 +32,33 @@ export class InviteCandidateComponent implements OnInit {
     public dialogRef: MatDialogRef<InviteCandidateComponent>,
     private projService: ProjectCRUDService,
     @Inject(MAT_DIALOG_DATA) public SendInviteData,
-    private _snackBar: MatSnackBar
+    private _snackBar: MatSnackBar,
+    private formBuilder: FormBuilder
   ) {}
 
-  invitationForm: FormGroup = new FormGroup({
-    candidateMessage: new FormControl('', [Validators.required]),
-  });
-
-  onSubmit() {
+  invite() {
     var formData = {
       userId: this.userID,
       projectId: this.SendInviteData.projectId,
       inviteeId: this.SendInviteData.inviteeId,
-      message: this.invitationForm.value.candidateMessage,
+      message: this.invitationFormGroup.value.invitationMessage,
     };
 
     console.log('About to Invite the Candidate!');
     console.log(formData);
 
-    this.projService.inviteCandidate(formData).subscribe((data) => {
-      if (data[Object.keys(data)[0]] == true)
-        this._snackBar.open('Successfully Applied for Project!', '', {
-          duration: 3000,
-        });
-      else {
-        this._snackBar.open('Project Application Failed.', '', {
-          duration: 3000,
-        });
-      }
-      console.log('Response: ', data);
-    });
+    // this.projService.inviteCandidate(formData).subscribe((data) => {
+    //   if (data[Object.keys(data)[0]] == true)
+    //     this._snackBar.open('Successfully Applied for Project!', '', {
+    //       duration: 3000,
+    //     });
+    //   else {
+    //     this._snackBar.open('Project Application Failed.', '', {
+    //       duration: 3000,
+    //     });
+    //   }
+    //   console.log('Response: ', data);
+    // });
 
     this.cancel();
   }
