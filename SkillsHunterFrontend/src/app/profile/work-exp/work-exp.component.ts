de3dc39d2e21bf1cb33ffbe41d332e9725909686
Thuso-Khoza <u17088799@tuks.co.za>
2021-09-17 17:35:36 -0700

    Update work-exp.component.ts

--- SkillsHunterFrontend/src/app/profile/work-exp/work-exp.component.ts
+++ SkillsHunterFrontend/src/app/profile/work-exp/work-exp.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators, FormBuilder } from '@angular/forms';
 import { MatDialogRef,MatDialog, MatDialogConfig,MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Inject } from '@angular/core';
-
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { DateAdapter } from '@angular/material/core';
 @Component({
   selector: 'app-work-exp',
   templateUrl: './work-exp.component.html',
@@ -10,7 +11,9 @@ import { Inject } from '@angular/core';
 })
 export class WorkExpComponent implements OnInit {
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data,public dialogRef: MatDialogRef<WorkExpComponent>,private dialog: MatDialog) { }
+  constructor(private dateAdapter: DateAdapter<Date>,@Inject(MAT_DIALOG_DATA) public data,public dialogRef: MatDialogRef<WorkExpComponent>,private dialog: MatDialog) { 
+    this.dateAdapter.setLocale('en-GB'); //dd/MM/yyyy
+  }
 
   empty=true;//show if this is new data being created or existing being edited. true means new data
   
@@ -24,13 +27,13 @@ export class WorkExpComponent implements OnInit {
       this.workDetailsForm.controls['organisation'].setValue(d.organisation);
       this.workDetailsForm.controls['role'].setValue(d.role);
       this.workDetailsForm.controls['description'].setValue(d.description);
-      this.workDetailsForm.controls['duration'].setValue(d.duration);
-      this.workDetailsForm.controls['time'].setValue(d.time);
+      this.workDetailsForm.controls['start'].setValue(d.realStart);
+      this.workDetailsForm.controls['end'].setValue(d.realEnd);
     }
 
   }
-
-dropList: string[] = ['Day(s)', 'Month(s)','Year(s)'];
+  
+//dropList: string[] = ['Day(s)', 'Month(s)','Year(s)'];
   workDetailsForm = new FormGroup({
     organisation: new FormControl('', [
       Validators.required,
@@ -42,17 +45,18 @@ dropList: string[] = ['Day(s)', 'Month(s)','Year(s)'];
     ]),
     description: new FormControl('', [
       Validators.required,
-      Validators.pattern('^[a-zA-Z ]*$'),
+      Validators.pattern('^[a-zA-Z ]*$'), 
     ]),
-    duration: new FormControl('', [Validators.required,Validators.pattern('[- +()0-9]+')]),
-    time:new FormControl('',[Validators.required]),
-  
+    //duration: new FormControl('', [Validators.required,Validators.pattern('[- +()0-9]+')]),
+    //time:new FormControl('',[Validators.required]),
+    start: new FormControl('', [Validators.required]),
+    end: new FormControl('' )
   });
 
   onSubmit(){
     console.log("clicked work");
     if(!this.workDetailsForm.controls['organisation'].invalid && !this.workDetailsForm.controls['role'].invalid && 
-    !this.workDetailsForm.controls['description'].invalid && !this.workDetailsForm.controls['duration'].invalid && !this.workDetailsForm.controls['time'].invalid){
+    !this.workDetailsForm.controls['description'].invalid && !this.workDetailsForm.controls['start'].invalid ){
       console.log("valid");
       this.workDetailsForm.controls['organisation'].value
 
@@ -60,8 +64,8 @@ dropList: string[] = ['Day(s)', 'Month(s)','Year(s)'];
         organisation:this.workDetailsForm.controls['organisation'].value,
         role:this.workDetailsForm.controls['role'].value,
         description:this.workDetailsForm.controls['description'].value,
-        duration:this.workDetailsForm.controls['duration'].value,
-        time:this.workDetailsForm.controls['time'].value
+        start:this.workDetailsForm.controls['start'].value,
+        end:this.workDetailsForm.controls['end'].value
       }});
     }
   }
