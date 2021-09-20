73ca9700197002cb4064463d33b279124bfb66d1
Tshepang <u17152837@tuks.co.za>
2021-09-20 15:08:07 +0200

    Changes

--- SkillsHunterFrontend/src/app/applications-and-invitations-recruiter/applications-and-invitations-recruiter.component.ts
+++ SkillsHunterFrontend/src/app/applications-and-invitations-recruiter/applications-and-invitations-recruiter.component.ts
@@ -1,15 +1,67 @@
 import { Component, OnInit } from '@angular/core';
+import { ProjectCRUDService } from '../services/project-crud.service';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-applications-and-invitations-recruiter',
   templateUrl: './applications-and-invitations-recruiter.component.html',
-  styleUrls: ['./applications-and-invitations-recruiter.component.scss']
+  styleUrls: ['./applications-and-invitations-recruiter.component.scss'],
 })
 export class ApplicationsAndInvitationsRecruiterComponent implements OnInit {
+  constructor(private projectCrud: ProjectCRUDService) {}
 
-  constructor() { }
+  arrRecruiterInvitationsReceived = [];
+  arrRecruiterApplicationsSent = [];
+
+  selected_project = 'a62d3f98-d227-452c-0b46-08d97c0cc7a2';
 
   ngOnInit(): void {
+    this.projectCrud
+      .getApplicationsForProject(this.selected_project)
+      .subscribe((data) => {
+        console.log(data);
+        this.arrRecruiterApplicationsSent = data;
+      });
+
+    this.projectCrud
+      .getInvitationsForProject(this.selected_project)
+      .subscribe((data) => {
+        console.log(data);
+        this.arrRecruiterInvitationsReceived = data;
+      });
   }
 
+  displayedColumnsApplications: string[] = [
+    'application_date',
+    'application_message',
+    'application_response',
+  ];
+  dataSourceApplications = new MatTableDataSource(
+    this.arrRecruiterApplicationsSent
+  );
+
+  displayedColumnsInvitations: string[] = [
+    'invitation_date',
+    'invitation_message',
+    'invitation_response',
+  ];
+  dataSourceInvitations = new MatTableDataSource(
+    this.arrRecruiterInvitationsReceived
+  );
+
+  applyFilterApplications(filterValue: string) {
+    this.dataSourceApplications.filter = filterValue.trim().toLowerCase();
+  }
+
+  applyFilterInvitations(filterValue: string) {
+    this.dataSourceApplications.filter = filterValue.trim().toLowerCase();
+  }
+
+  showInvitation(element: any) {
+    console.log(element);
+  }
+
+  showApplication(element: any) {
+    console.log(element);
+  }
 }
