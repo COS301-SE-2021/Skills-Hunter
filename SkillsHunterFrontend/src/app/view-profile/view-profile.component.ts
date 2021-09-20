5e504c5b228e3126da7d5cb5e43a766541ea9e95
Thuso-Khoza <u17088799@tuks.co.za>
2021-09-20 10:35:52 -0700

    Update view-profile.component.ts
    retrieve data

--- SkillsHunterFrontend/src/app/view-profile/view-profile.component.ts
+++ SkillsHunterFrontend/src/app/view-profile/view-profile.component.ts
@@ -10,19 +10,19 @@ import { AdminService } from '../services/admin.service';
 export class ViewProfileComponent implements OnInit {
 
   constructor(@Inject(MAT_DIALOG_DATA) public data,private user: AdminService) { }
-
+  userData;
   imageUrl = "/assets/images/profile.png";
   ngOnInit(): void {
  
     if(this.data!=""){
       console.log("in slider: "+this.data);
     
-  this.user.getUser(this.data.id)
+  this.user.getUser("ef28e43c-b8e7-492b-880a-08d97c54598a")//this.data.id)
   .subscribe(
     data=>{
      
-      console.log('Response post', data);
-     
+      console.log('Response post', data.name);
+      this.userData=data;
     }
   );
     }
