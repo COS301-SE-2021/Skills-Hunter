6e1ce71f983d184f86f590a88bfb634535199c17
Sboniso Masilela <u10416260@tuks.co.za>
2021-09-25 23:26:41 +0200

    Added GetCollectionResponse class, GetAllSkillCollectionsQuery and GetCategoryByIdQuery class

---
+++ SkillsHunterAPI/Queries/GetAllSkillCollectionsQuery.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using SkillsHunterAPI.Models.Skill;
+using SkillsHunterAPI.Models.Skill.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SkillsHunterAPI.Queries
+{
+    public class GetAllSkillCollectionsQuery : IRequest<GetSkillCollectionResponse>
+    {
+    }
+}
---
+++ SkillsHunterAPI/Queries/GetCategoryByIdQuery.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using SkillsHunterAPI.Models.Skill;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SkillsHunterAPI.Queries
+{
+    public class GetCategoryByIdQuery : IRequest<GetCategoryResponse>
+    {
+
+
+        public Guid Id { get; }
+        public GetCategoryByIdQuery(Guid id)
+        {
+            Id = id;
+        }
+    }
+}
