288afc30c03ab650827bf68b21f14e91f9bbefe4
Sboniso Masilela <u10416260@tuks.co.za>
2021-09-25 20:28:15 +0200

    Updated get skills query for response object

--- SkillsHunterAPI/Queries/GetSkillsQuery.cs
+++ SkillsHunterAPI/Queries/GetSkillsQuery.cs
@@ -1,11 +1,13 @@
-﻿using System;
+﻿using MediatR;
+using SkillsHunterAPI.Models.Skill;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace SkillsHunterAPI.Queries
 {
-    public class GetSkillsQuery : IRequest<>
+    public class GetSkillsQuery : IRequest<GetSkillsResponse>
     {
     }
 }
