432325becadcf20cd5dbcf4c6bb319dae5a7336f
Sboniso Masilela <u10416260@tuks.co.za>
2021-09-26 00:54:15 +0200

    Implemented AddCategoryHandler class

---
+++ SkillsHunterAPI/Handlers/AddCategoryHandler.cs
@@ -0,0 +1,40 @@
+﻿using MediatR;
+using SkillsHunterAPI.Models.Skill;
+using SkillsHunterAPI.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SkillsHunterAPI.Handlers
+{
+    public class AddCategoryHandler : IRequestHandler<AddCategoryCommand, GetCategoryResponse>
+    {
+        private IAdminService _adminService;
+
+        public AddCategoryHandler(IAdminService adminService)
+        {
+            _adminService = adminService;
+        }
+
+        public async Task<GetCategoryResponse> Handle(AddCategoryCommand request, CancellationToken cancellationToken)
+        {
+            // Add category code here
+            Category category = new Category();
+
+            category.Name = request.Name;
+            category.Description = request.Description;
+
+            var result = await _adminService.AddCategory(category);
+
+            GetCategoryResponse response = new GetCategoryResponse();
+
+            response.Id = result.CategoryId;
+            response.Name = result.Name;
+            response.Description = result.Description;
+
+            return response;
+        }
+    }
+}
