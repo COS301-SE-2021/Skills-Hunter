78ad1e958015383c8c993bf334f8de682bf0bfcf
Sboniso Masilela <u10416260@tuks.co.za>
2021-09-25 23:52:48 +0200

    Implemented GetSkillByIdHandler method and GetSkillByIdQuery

---
+++ SkillsHunterAPI/Queries/GetSkillByIdQuery.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using SkillsHunterAPI.Models.Skill;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SkillsHunterAPI.Queries
+{
+    public class GetSkillByIdQuery : IRequest<GetSkillResponse>
+    {
+        public Guid Id { get; }
+
+        public GetSkillByIdQuery(Guid id)
+        {
+            Id = id;
+        }
+
+    }
+}
