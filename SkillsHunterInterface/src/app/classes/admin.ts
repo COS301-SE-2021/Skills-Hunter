f062cb8c7116e25bcabc810950948c1dd4d37efb
Sboniso Masilela <u10416260@tuks.co.za>
2021-09-27 00:14:11 +0200

    Added admin classes

--- SkillsHunterInterface/src/app/classes/admin.ts
+++ SkillsHunterInterface/src/app/classes/admin.ts
@@ -1,8 +1,29 @@
 //Entity
-
+export interface skill{
+    skillId: string,
+    name: string,
+    status: number
+}
 
 
 //Requets
+export interface createSkillRequest{
+    name: string,
+    categories: getCategoryId[]
+}
 
+export interface createCategoryRequest{
+    name: string,
+    description: string
+}
 
 //Responses
+
+export interface getSkillsResponse{
+    skills: skill[]
+}
+
+//Helpers
+export interface getCategoryId{
+    categoryId: string
+}
