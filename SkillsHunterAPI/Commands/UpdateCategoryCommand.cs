7eb617938a23fdeffb0c34a48222e94e5c4f2952
Sboniso Masilela <u10416260@tuks.co.za>
2021-09-26 15:25:38 +0200

    Implemented UpdateCategoryHandler class and UpdateCategoryCommand class

---
+++ SkillsHunterAPI/Commands/UpdateCategoryCommand.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using SkillsHunterAPI.Models.Skill;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SkillsHunterAPI.Commands
+{
+    public class UpdateCategoryCommand : IRequest<UpdateCategoryResponse>
+    {
+        [Required(ErrorMessage = "Id is required")]
+        public String Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
