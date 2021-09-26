4fb0f6abc5e8165c494a74ee056a1c3ac779f28c
Sboniso Masilela <u10416260@tuks.co.za>
2021-09-26 11:16:15 +0200

    Implemented CreateSkillHandler

---
+++ SkillsHunterAPI/Handlers/CreateSkillHandler.cs
@@ -0,0 +1,42 @@
+﻿using MediatR;
+using SkillsHunterAPI.Models.Skill;
+using SkillsHunterAPI.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SkillsHunterAPI.Handlers
+{
+    public class CreateSkillHandler : IRequestHandler<AddSkillCommand, GetSkillResponse>
+    {
+
+        private IAdminService _adminService;
+
+        public CreateSkillHandler(IAdminService adminService)
+        {
+            _adminService = adminService;
+        }
+
+        public async Task<GetSkillResponse> Handle(AddSkillCommand request, CancellationToken cancellationToken)
+        {
+            Skill skill = new Skill();
+            skill.Name = request.Name;
+            skill.Status = SkillStatus.Accepted;
+
+            var result = await _adminService.CreateSkill(skill);
+
+            //Link skill with Categories
+            await _adminService.AddCategoriesToSkill(skill.SkillId, request.Categories);
+
+            //Create response
+            GetSkillResponse response = new();
+            response.Id = result.SkillId;
+            response.Name = result.Name;
+            response.Status = result.Status;
+
+            return response;
+        }
+    }
+}
