53f9fe6e413c56e51ee2d54163c507e5644b01e5
Sboniso Masilela <u10416260@tuks.co.za>
2021-09-26 15:26:45 +0200

    Implemented RemoveSkillHandler class and RemoveSkillCommand

---
+++ SkillsHunterAPI/Commands/RemoveSkillCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using SkillsHunterAPI.Models.Skill;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SkillsHunterAPI.Commands
+{
+    public class RemoveSkillCommand : IRequest<RemoveSkillResponse>
+    {
+        public Guid SkillId { get; set; }
+    }
+}
