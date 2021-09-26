3c085bd68d692f04e6b9e1d1ceaeba2b2d30604f
MxolisiMkanzi <u18059326@tuks.co.za>
2021-09-27 01:18:07 +0200

    Added classes that are required by the project functionalities

---
+++ SkillsHunterInterface/src/app/classes/project.ts
@@ -0,0 +1,107 @@
+import { getCategoryId, getSkillId } from "./admin";
+
+//Entity
+export interface project{
+    projectId: string,
+    name: string,
+    description: string,
+    owner: string,
+    location: string,
+    openForApplications: boolean,
+    dateCreated: Date,
+    skills: projectSkillAndCollection
+}
+
+export interface projectSkill{
+    projectSkillId: string,
+    skillId: string,
+    name: string,
+    weight: number
+}
+
+export interface projectSkillCollection{
+    projectSkillCollectionId: string,
+    name: string,
+    description: string,
+    weight: number,
+    skills: projectSkill[]
+}
+
+//Request
+export interface createProjectRequest{
+    name: string,
+    description: string,
+    location: string,
+    openForApplications: boolean,
+    existingSkills: existingSkill[],
+    newSkills: newSkill[],
+    skillCollections: createCollection[]
+}
+
+export interface inviteCandidateRequest{
+    projectId: string,
+    inviteeId: string,
+    message: string
+}
+
+//Response
+export interface matchingCandidate{
+    percentage: number,
+    userId: string,
+    name: string,
+    surname: string,
+    email: string,
+    matchingSkills: matchingSkill[]
+}
+
+export interface getApplications{
+    percentage: number,
+    userId: string,
+    name: string,
+    surname: string,
+    email: string,
+    applicationDate: Date
+    matchingSkills: matchingSkill[]
+}
+
+export interface getInvitationResponse{
+    invitationId: string,
+    inviteeId: string,
+    inviterId: string,
+    projectId: string,
+    inviteDate: Date
+    status: number,
+    message: string
+}
+
+//Helpers
+export interface projectSkillAndCollection{
+    skills: projectSkill[],
+    skillCollections: projectSkillCollection[]
+}
+
+export interface existingSkill{
+    skillId: string,
+    weight: number
+}
+
+export interface newSkill{
+    name: string,
+    categories: getCategoryId[],
+    weight: number
+}
+
+export interface createCollection{
+    name: string,
+    description: string,
+    weight: number,
+    skills: getSkillId[]
+}
+
+export interface matchingSkill{
+    skillId: string,
+    name: string,
+    weight: number,
+    percentage: number,
+    yearsOfExperience: number
+}
