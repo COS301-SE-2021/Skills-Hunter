c0df0e3fa338f201af1fae8f4274aff76d179de7
MxolisiMkanzi <u18059326@tuks.co.za>
2021-09-30 00:57:05 +0200

    fixed bugs

--- SkillsHunterAPI/Commands/Project/InviteCandidateCommand.cs
+++ SkillsHunterAPI/Commands/Project/InviteCandidateCommand.cs
@@ -3,14 +3,14 @@ using MediatR;
 
 namespace SkillsHunterAPI.Commands.Project
 {
-    public class inviteCandidateCommand : IRequest<bool>
+    public class InviteCandidateCommand : IRequest<bool>
     {
         public Guid UserId { get; }
         public Guid ProjectId { get; }
         public Guid InviteeId { get; }
         public string Message { get; }
 
-        public inviteCandidateCommand(Guid user, Guid project, Guid invitee, string mes)
+        public InviteCandidateCommand(Guid user, Guid project, Guid invitee, string mes)
         {
             UserId = user;
             ProjectId = project;
