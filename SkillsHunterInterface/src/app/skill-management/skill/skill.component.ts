5b40043f6cf707fe3ab99527453a5bb0480b164e
Sboniso Masilela <u10416260@tuks.co.za>
2021-10-06 02:47:51 +0200

    Implemented skill component

--- SkillsHunterInterface/src/app/skill-management/skill/skill.component.ts
+++ SkillsHunterInterface/src/app/skill-management/skill/skill.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { skill } from 'src/app/classes/admin';
+import { MatDialog , MatDialogConfig } from '@angular/material/dialog';
+import { EditSkillComponent } from '../edit-skill/edit-skill.component'; 
+
 
 @Component({
   selector: 'app-skill',
@@ -7,9 +11,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SkillComponent implements OnInit {
 
-  constructor() { }
+  @Input() skill:skill;
+  @Output() onDeleteUser: EventEmitter<skill> = new EventEmitter();
+  constructor(public dialog: MatDialog) { }
 
   ngOnInit(): void {
+  }
+
+  getCategory(id) : string{
+
+    let result:string = "";
+
+    return result;
+  }
+
+  onDelete(skill): void{
+    this.onDeleteUser.emit(skill);
+  }
+
+  onEdit(): void{
+    const configDialog = new MatDialogConfig();
+    configDialog.backdropClass = 'backGround';
+    configDialog.width = '40%';
+    configDialog.height = '70%';
+    configDialog.data = this.skill;
+
+    const dialogRef = this.dialog.open(EditSkillComponent,configDialog);
+
+    dialogRef.afterClosed().subscribe(() => {
+      this.ngOnInit();
+    });
   }
 
 }
